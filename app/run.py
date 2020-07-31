2444ef7bcf90ef229baeb33cfb862f433b767f38
Chris Rowles <me@rowles.ch>
2020-07-31 18:18:50 +0100

    rename

--- app/run.py
+++ app/run.py
@@ -1,5 +1,5 @@
 from logger import Logger
-from shell import Tornamiko
+from tornamiko import Tornamiko
 
 if '__main__' == __name__:
     logger = Logger(path="/home/chris/PycharmProjects/raspi-shell/data/history")
